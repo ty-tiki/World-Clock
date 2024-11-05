40254309d688075c8cc213b31ad9b62819fb0be5
ty-tiki <young.toni@gmail.com>
2024-11-05 11:30:26 +0000

    Added in the current location

--- src/index.js
+++ src/index.js
@@ -57,6 +57,9 @@ function updateTime() {
 
 function changeCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let allCitiesElement = document.querySelector("#all-cities");
